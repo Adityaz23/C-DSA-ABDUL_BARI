fd62300ef28d9fc77b3ad0fda1e17081ab473eeb
Adityaz <apple@Adityaz.local>
2025-11-19 00:57:57 +0530

    Completed the array section

--- Section-1/Arrays/Arrays/main.cpp
+++ Section-1/Arrays/Arrays/main.cpp
@@ -9,11 +9,25 @@
 using namespace std;
 int main() {
     // insert code here...
-    int A[4] = {1,2,3,4};
-    for (int i=0; i<=4; i++) {
-        cout << A[i] << " ";
+    //    int A[10] = {1,2,3,4};
+    //    int B[10] = {0};
+    //    for (int i=0; i<=10; i++) {
+    //        cout << A[i] << " " << endl;
+    //    }
+    // using for each loop to access all the elements in the array.
+    //    for(int x: A){
+    //        cout << x << endl;
+    //    }
+    int n;
+    cout << "Enter the size of an array: ";
+    cin >> n;
+    int A[n];
+    A[0] = 3;
+    for(int x: A){
+        cout << x <<  endl;
     }
-    cout << "Hello, World!" << " ";
-    cout<<sizeof(A);
+   
+//    printf("%d\n",A[2]); // We can also use the print f function in the cpp language.
+//    cout << B << endl;
     return 0;
 }
